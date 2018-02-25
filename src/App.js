f50fb300e130de632355983df2eab7f6f24534ed
ArtemeeSenin <artemee.senin@gmail.com>
2018-02-25 17:18:35 +0300

    routing App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Info from './components/Info';
@@ -6,13 +7,9 @@ import Info from './components/Info';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Info />
-      </div>
+      <Router>
+        <Route path="/" component{} />
+      </Router>
     );
   }
 }
